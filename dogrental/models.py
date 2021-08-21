12ee9fbe5dd32b6ebd1503739a603f4b6cd870ce
madison <madisonamoa@gmail.com>
2021-08-21 01:12:56 +0000

    define relationship

---
+++ dogrental/models.py
@@ -0,0 +1,45 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from .database import Base
+from sqlalchemy.orm import relationship
+
+class User(Base):
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    username = Column(String, unique=True)
+    email = Column(String)
+    passord = Column(String)
+
+    tenants = relationship("Tenant", back_populates="creator")
+
+class Dog(Base):
+
+    __tablename__ = 'dogs'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    race = Column(String, default="unknown")
+
+    tenant_id = Column(Integer, ForeignKey('tenant.id'))
+
+    owner = relationship("Tenant", back_populates="dogs")
+
+class Tenant(Base):
+
+    __tablename__ = 'tenants'
+
+    id = Column(Integer, primary_key=True, index=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    email = Column(String)
+    adress = Column(String)
+
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    creator = relationship("User", back_populates="tenants")
+
+    dogss = relationship("Dog", back_populates="owner")
+
