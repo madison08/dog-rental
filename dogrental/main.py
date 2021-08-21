433b3fd561df4f989033d80647b91053949151ad
madison <madisonamoa@gmail.com>
2021-08-21 01:15:58 +0000

    create databae engine

--- dogrental/main.py
+++ dogrental/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 
+from . import models
+from .database import engine 
 
 app = FastAPI(title="dog rental API")
+
+
+models.Base.metadata.create_all(engine)
 
 
 @app.get("/")
@@ -9,4 +14,3 @@ def index():
 
     return {'infos': 'welcome'}
 
-    
